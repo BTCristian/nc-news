3e31cb5879513b0bd8fbc5b104432f7ac12186d0
Tiberiu Badea <tcristian.b@gmail.com>
2024-01-15 16:46:22 +0000

    Task 3 done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,19 @@ afterAll(() => {
   return db.end();
 });
 
+describe("/api", () => {
+  test("GET 200: should provide a list with all endpoints (with description) available as an object", () => {
+    const endpoints = require("../endpoints.json");
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body).toEqual(endpoints);
+      });
+  });
+});
+
 describe("/api/topics", () => {
   test("GET 200: should return an array of topics objects ", () => {
     return request(app)
@@ -23,7 +36,6 @@ describe("/api/topics", () => {
         expect(response.body.topic.length).toBeGreaterThan(0);
         expect(Array.isArray([response])).toBe(true);
         response.body.topic.forEach((topic) => {
-          console.log(topic);
           expect(topic).toHaveProperty("description");
           expect(topic).toHaveProperty("slug");
         });
@@ -35,7 +47,6 @@ describe("/api/topics", () => {
       .get("/api/nonsense")
       .expect(404)
       .then((response) => {
-        // console.log(response.res.statusMessage);
         expect(response.res.statusMessage).toBe("Not Found");
       });
   });
