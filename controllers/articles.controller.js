f6e8851a0add15e7a291139f3a7fffc380ba3249
Tiberiu Badea <tcristian.b@gmail.com>
2024-01-18 14:53:01 +0000

    Task 8 done

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -3,6 +3,7 @@ const {
   fetchAllArticles,
   fetchCommentsByArticleId,
   insertComment,
+  updateArticleVotes,
 } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
@@ -43,10 +44,33 @@ exports.postCommentByArticleId = (req, res, next) => {
       .status(400)
       .send({ msg: "Username or body are required fields" });
   }
+  // let these undefined values go into the query, in which case
+  // PSQL would throw an error which can be handled.
+  // this approach also works^^
 
   return insertComment({ article_id, author: username, body })
     .then((comment) => {
       res.status(201).send({ comment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+
+  if (typeof inc_votes !== "number") {
+    return res
+      .status(400)
+      .send({ msg: "Bad request, inc_votes must be a number" });
+  }
+
+  updateArticleVotes(article_id, inc_votes)
+    .then(() => fetchArticleById(article_id))
+    .then((updatedArticle) => {
+      res.status(200).send({ article: updatedArticle });
     })
     .catch((err) => {
       next(err);
