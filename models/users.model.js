07199194b7940a4e413986d28b0bb21360b0c55c
Cristian B <147935406+BTCristian@users.noreply.github.com>
2024-03-16 12:47:15 +0000

    Update users.model.js

--- models/users.model.js
+++ models/users.model.js
@@ -1,18 +1,14 @@
 const db = require("../db/connection");
 
-exports.selectUsers = () => {
-  return db.query(`SELECT * FROM users;`).then((results) => {
-    return results.rows;
-  });
-};
-
-exports.selectByUsername = (username) => {
+exports.fetchAllUsers = () => {
   return db
-    .query(`SELECT * FROM users WHERE username = $1;`, [username])
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({ status: 404, message: "User Does Not Exist" });
-      }
-      return result.rows[0];
+    .query(
+      `
+            SELECT username,name,avatar_url
+            FROM users
+            `
+    )
+    .then((results) => {
+      return results.rows;
     });
 };
