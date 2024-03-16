4b19011acea69863d7de41ac2f8097cb17050352
Cristian B <147935406+BTCristian@users.noreply.github.com>
2024-03-16 12:44:22 +0000

    Update topics.controller.js

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,31 +1,11 @@
-const {
-  selectTopics,
-  selectEndpoints,
-  addTopic,
-} = require("../models/topics.model");
+const { fetchAllTopics } = require("../models/topics.model");
 
-exports.getTopics = (req, res, next) => {
-  selectTopics()
-    .then((topics) => {
-      res.status(200).send({ topics });
-    })
-    .catch(next);
-};
-
-exports.getEndpoints = (req, res, next) => {
-  selectEndpoints()
-    .then((endpoints) => {
-      res.status(200).send({ endpoints });
-    })
-    .catch(next);
-};
-
-exports.postTopics = (req, res, next) => {
-  const newTopic = req.body;
-
-  addTopic(newTopic)
+exports.getAllTopics = (req, res, next) => {
+  fetchAllTopics()
     .then((topic) => {
-      res.status(201).send({ topic });
+      res.status(200).send({ topic });
     })
-    .catch(next);
+    .catch((err) => {
+      next(err);
+    });
 };
