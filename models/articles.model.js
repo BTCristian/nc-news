de640c9e0afe094180243398af604a54e81d729d
Cristian B <147935406+BTCristian@users.noreply.github.com>
2024-03-16 12:46:11 +0000

    Update articles.model.js

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,149 +1,216 @@
 const db = require("../db/connection");
 
-exports.selectArticleById = (article_id) => {
+exports.fetchArticleById = (article_id) => {
   return db
     .query(
-      `SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, articles.votes, articles.article_img_url, articles.body, COUNT(comments.comment_id) AS comment_count
+      `
+      SELECT
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      COUNT(comments.comment_id) AS comment_count
     FROM articles
     LEFT JOIN comments ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
-    GROUP BY articles.article_id;`,
+    GROUP BY articles.article_id;
+    `,
       [article_id]
     )
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          message: "Article Does Not Exist",
-        });
+    .then(({ rows }) => {
+      if (rows.length !== 0) {
+        return rows[0];
       }
-      return result.rows[0];
+      return Promise.reject({
+        status: 404,
+        msg: "Article ID not found",
+      });
     });
 };
 
-exports.selectArticles = (topic, sort_by = "created_at", order = "DESC") => {
-  const validQueries = [
-    "created_at",
-    "article_id",
-    "title",
-    "author",
-    "votes",
-    "comment_count",
-  ];
-
-  if (!validQueries.includes(sort_by)) {
-    return Promise.reject({ status: 404, message: "Invalid sort_by Query" });
-  }
-
-  let queryString = `SELECT articles.article_id, articles.title, articles.author, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url ,
-COUNT(comments.comment_id) AS comment_count
-FROM articles
-LEFT JOIN comments ON articles.article_id = comments.article_id`;
-
-  const queryParams = [];
-
+exports.fetchAllArticles = (topic) => {
+  let queryString = `
+  SELECT
+    articles.author,
+    articles.title,
+    articles.article_id,
+    articles.topic,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+  COUNT(comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN
+    comments ON articles.article_id = comments.article_id 
+  `;
   if (topic) {
-    queryString += `  WHERE topic = $1`;
-    queryParams.push(topic);
+    (queryString += `
+    WHERE articles.topic = $1
+    `),
+      [topic];
   }
 
   queryString += `
-GROUP BY articles.article_id`;
-
-  if (sort_by === "comment_count") {
-    queryString += `
-ORDER BY comment_count ${order};`;
-  } else {
-    queryString += `
-ORDER BY articles.${sort_by} ${order};`;
-  }
-
-  return db.query(queryString, queryParams).then((results) => {
+  GROUP BY
+    articles.article_id
+  ORDER BY
+    articles.created_at DESC
+    `;
+  return db.query(queryString, topic ? [topic] : []).then((results) => {
+    if (results.rows.length === 0) {
+      return db
+        .query(
+          `
+        SELECT * FROM topics
+        WHERE topics.slug = $1
+        `,
+          [topic]
+        )
+        .then(({ rows }) => {
+          if (rows.length === 0) {
+            return Promise.reject({
+              status: 404,
+              msg: `No articles found with topic: ${topic}`,
+            });
+          }
+          return [];
+        });
+    }
     return results.rows;
   });
 };
 
-exports.updateArticleById = (article_id, body) => {
-  const { inc_votes } = body;
-
-  if (Object.keys(body).length === 0) {
-    return db
-      .query(`SELECT * FROM articles WHERE article_id = ${article_id} `)
-      .then((result) => {
-        return result.rows[0];
-      });
-  }
-  if (!inc_votes) {
-    return Promise.reject({ status: 400, message: "Invalid Patch Query" });
-  }
-
+exports.fetchCommentsByArticleId = (article_id) => {
   return db
     .query(
       `
-    UPDATE articles
-  SET votes = votes + $1
-  WHERE article_id = ${article_id} 
-  RETURNING *`,
-      [inc_votes]
+  SELECT *
+  FROM comments
+  WHERE article_id = $1
+  ORDER BY created_at DESC
+  `,
+      [article_id]
     )
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          message: "Article Does Not Exist",
-        });
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return db
+          .query(
+            `
+        SELECT * FROM articles 
+        WHERE article_id = $1`,
+            [article_id]
+          )
+          .then(({ rows }) => {
+            if (rows.length === 0) {
+              return Promise.reject({
+                status: 404,
+                msg: "Article ID not found",
+              });
+            }
+            return [];
+          });
       }
-      return result.rows[0];
+      return rows;
     });
 };
 
-exports.addArticle = (newArticle) => {
-  const { author, title, body, topic, article_img_url } = newArticle;
-
-  let queryString = `INSERT INTO articles (author, title, body, topic`;
-  const queryParams = [author, title, body, topic];
-
-  if (article_img_url) {
-    queryString += ` , article_img_url) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
-    queryParams.push(article_img_url);
-  } else {
-    queryString += `) VALUES ($1, $2, $3, $4) RETURNING *;`;
-  }
-
+//to be refactored considering insert query error
+// (both article_id and author are foreign keys REFERENCING other tables.
+//   So you don't need to do any manual checking that they exist,
+//   you can use the one POST query and if they don't exist PSQL
+//   will throw an error automatically.)
+// from here:
+// vvv
+const checkIfUserExists = (author) => {
   return db
-    .query(queryString, queryParams)
-    .then((result) => {
-      return db.query(
-        `SELECT articles.*, 
-    COUNT(comments.comment_id) AS comment_count
-    FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    WHERE articles.article_id = $1
-    GROUP BY articles.article_id`,
-        [result.rows[0].article_id]
-      );
-    })
-    .then((result) => {
-      return result.rows[0];
+    .query(
+      `
+      SELECT * FROM users 
+      WHERE username = $1
+      `,
+      [author]
+    )
+    .then(({ rows }) => {
+      return rows.length > 0;
     });
 };
 
-exports.removeArticleById = (article_id) => {
+const checkIfArticleExists = (article_id) => {
   return db
-    .query(`DELETE FROM comments WHERE article_id = $1 RETURNING *;`, [
-      article_id,
-    ])
-    .then(() => {
-      return db.query(
-        `DELETE FROM articles WHERE article_id = $1 RETURNING *;`,
-        [article_id]
-      );
-    })
-    .then((result) => {
-      if (result.rows.length === 0) {
+    .query(
+      `
+      SELECT * FROM articles 
+      WHERE article_id = $1
+      `,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      return rows.length > 0;
+    });
+};
+
+exports.insertComment = ({ article_id, author, body }) => {
+  const checkUser = checkIfUserExists(author);
+  const checkArticle = checkIfArticleExists(article_id);
+
+  return Promise.all([checkUser, checkArticle]).then((data) => {
+    if (!data[0]) {
+      return Promise.reject({ status: 400, msg: "User not found" });
+    }
+    if (!data[1]) {
+      return Promise.reject({ status: 404, msg: "Article not found" });
+    }
+
+    return db
+      .query(
+        `
+    INSERT INTO comments (article_id, author, body)
+    VALUES ($1,$2,$3)
+    RETURNING *
+    `,
+        [article_id, author, body]
+      )
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          return Promise.reject({ status: 404, msg: "Article ID not found" });
+        }
+        return rows[0];
+      });
+  });
+};
+
+//to here ^^^
+
+exports.updateArticleVotes = (article_id, inc_votes) => {
+  return db.query(
+    `
+      UPDATE articles
+      SET votes = votes + $1
+      WHERE article_id = $2
+      RETURNING *
+      `,
+    [inc_votes, article_id]
+  );
+};
+
+exports.deleteCommentById = (comment_id) => {
+  return db
+    .query(
+      `
+    DELETE FROM comments
+    WHERE comment_id = $1
+    RETURNING *
+    `,
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
         return Promise.reject({
           status: 404,
-          message: "Article Does Not Exist",
+          msg: "Comment with provided ID not found",
         });
       }
     });
